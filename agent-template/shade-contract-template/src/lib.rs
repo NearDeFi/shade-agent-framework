2e7df54359127010f36d7960231ee28aead03d4c
PiVortex <owengeorgehassall@gmail.com>
2026-01-31 15:06:58 +0000

    move audit instructions

--- agent-template/shade-contract-template/src/lib.rs
+++ agent-template/shade-contract-template/src/lib.rs
@@ -1,3 +1,11 @@
+// Presume that attestation.verify method has no vulnerabilities 
+
+// Main things to look for in this project is
+// - An agent cannot register in tee mode if it doesnt have a correct attestation. 
+// - An agent cannot call the request_signature function if it doesnt have a currently approved ppid and measurements
+// - The same as the two above for local but it also requires it to be whitelisted 
+
+
 use hex;
 use near_sdk::{
     AccountId, BorshStorageKey, Gas, NearToken, PanicOnDefault, Promise,
